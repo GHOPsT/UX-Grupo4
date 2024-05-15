7f18454fb0daae52bbd85c4059c392b95467ceec
GHOPsT <mggsalvator@gmail.com>
2024-05-15 12:02:46 -0500

    Creating backend, frontend and login and registration pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InicioSesion from './pages/inicio_sesion'; // Asegúrate de que la ruta de importación sea correcta
+
+function App() {
+  return (
+    <div>
+      <InicioSesion />
+    </div>
+  );
+}
+
+export default App;
